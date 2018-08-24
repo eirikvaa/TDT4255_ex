b7dfca08d00725162831ff2a74cc46f7b328bcbf
Eirik Vale Aase <e.v.aase@gmail.com>
2018-08-24 12:01:46 +0200

    CyclicVecMat tests passes

--- src/main/scala/CyclicVecMat.scala
+++ src/main/scala/CyclicVecMat.scala
@@ -64,13 +64,85 @@ class CyclicVecMat(matrixDims: Dims, dataWidth: Int) extends Module {
   // Create counters to keep track of when the matrix and vector has gotten all the data.
   // You can assume that writeEnable will be synchronized with the vectors. I.e for a vector
   // of length 3 writeEnable can only go from true to false and vice versa at T = 0, 3, 6, 9 etc
-
+  val rowSelect = Counter(dims.rows)
+  val currentCol = Counter(dims.cols)
+  val vecADone = RegInit(Bool(), false.B)
+  val matrixBDone = RegInit(Bool(), false.B)
 
   // Create counters to keep track of how far along the computation is.
+  io.done := false.B
+  io.dataOut := 0.U
+  io.dataValid := false.B
+  dotProductCalculator.dataInA := 0.U
+  dotProductCalculator.dataInB := 0.U
+
+  // Initialize stuff so the compiler don't complain.
+  vecA.dataIn := 0.U
+  matrixB.dataIn := 0.U
+
+  // Set correct write enablel signals
+  vecA.writeEnable := io.writeEnableA
+  matrixB.writeEnable := io.writeEnableB
 
   // Set up the correct rowSelect for matrixB
+  matrixB.rowSelect := rowSelect.value
 
-  // Wire up write enables for matrixB and vecA
+  // Check if we're not done inputting data
+  when (dataIsLoaded === false.B) {
+	// Only input vector data if it's not done.
+	when (vecADone === false.B) {
+		vecA.dataIn := io.dataInA
+	}
+
+	// Only input matrix data if it's not done.
+	when (matrixBDone === false.B) {
+		matrixB.dataIn := io.dataInB
+	}
+  }
+
+  // Data has been entered into the current index for both the vector and the
+  // matrix, so we increment the current column.
+  when (currentCol.inc()) {
+	vecA.dataIn := io.dataInA
+	matrixB.dataIn := io.dataInB
+	vecADone := true.B
+
+	// We finished inputting the vector data, so don't do this anymore.
+	// Must reset the column index because we still have matrix rows to count.
+	currentCol.value := 0.U
+
+	// We'll check here if we just finished the last row. If not, increment the
+	// row count and start at it again.
+	when (rowSelect.inc()) {
+	  // The input vector is done, so now it's all inputting matrix numbers.
+	  matrixBDone := true.B
+
+	  // We're done with both vector and matrix input.
+	  dataIsLoaded := true.B
+	}
+  }
+
+  // Variables for the calculation
+  val colIndex = Counter(dims.cols)
+  val rowIndex = Counter(dims.rows)
+
+  when (dataIsLoaded) {
+	// Data is fully loaded, we must start calculating stuff now.
+	matrixB.rowSelect := rowIndex.value
+	dotProductCalculator.dataInA := vecA.dataOut
+	dotProductCalculator.dataInB := matrixB.dataOut
+	io.dataOut := dotProductCalculator.dataOut
+
+	when (colIndex.inc()) {
+	  // We done multiply vector with row n. Jump to row n + 1 in matrix.
+	  io.dataValid := true.B
+
+	  when (rowIndex.inc()) {
+		// We done multiplying all the shit.
+		io.done := true.B
+	  }
+	}
+  }
 
   /**
     In the solution I used the following to keep track of state
